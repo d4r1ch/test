e3d66f2927d7697704bd048aa78819d727c17da6
David Richter <d4r1ch@gmail.com>
2013-05-02 22:20:14 +0200

    První ulození

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokus2::Application.config.secret_token = '05ae9967dfa3a9f33a0c4338b1acae30ff142e0de0102b0b429ba3584784559ca489c01f008b749b624ed000721c87adeb284c3a351fc65d83fa41b6b49fd2e7'
